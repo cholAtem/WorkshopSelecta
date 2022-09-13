ae5a651e67be550ce3bf1dd2969361c1d429e509
Chol Atem <johnchol444@gmail.com>
2022-09-12 19:46:49 -0500

    Add project files.

---
+++ WorkshopSelecta.cs
@@ -0,0 +1,16 @@
+namespace WorkshopSelecta
+{
+    public partial class WorkshopSelecta : Krypton.Toolkit.KryptonForm
+    {
+        public WorkshopSelecta()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
